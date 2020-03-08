52edd753df2294dc270479344666cc6c39aa7070
shutd <shut.dmitry35@gmail.com>
2020-03-08 16:50:56 +0300

    improve resizing

--- model.py
+++ model.py
@@ -1,51 +1,64 @@
-# save the final model to file
+from __future__ import print_function
+import keras
 from keras.datasets import mnist
-from keras.utils import to_categorical, np_utils
 from keras.models import Sequential
-from keras.layers import Conv2D, Dropout
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.optimizers import SGD
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-# reshape to be [samples][width][height][channels]
-X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
-X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+batch_size = 128
+num_classes = 10
+epochs = 12
 
-def larger_model():
-	model = Sequential()
-	model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
-	model.add(MaxPooling2D())
-	model.add(Conv2D(15, (3, 3), activation='relu'))
-	model.add(MaxPooling2D())
-	model.add(Dropout(0.2))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu'))
-	model.add(Dense(50, activation='relu'))
-	model.add(Dense(num_classes, activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+# input image dimensions
+img_rows, img_cols = 28, 28
 
-# run the test harness for evaluating a model
-def run_test_harness():
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-	model = larger_model()
-	# Fit the model
-	model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
-	# Final evaluation of the model
-	scores = model.evaluate(X_test, y_test, verbose=0)
-	print("Large CNN Error: %.2f%%" % (100 - scores[1] * 100))
-	# save model
-	model.save('final_model.h5')
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
 
-# entry point, run the test harness
-run_test_harness()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+model.save('final_model.h5')
--- modelRunner.py
+++ modelRunner.py
@@ -3,35 +3,61 @@ from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
+import cv2
+
+
+def closiest_28(w, h):
+    i = 28
+    digit = 0
+    if w > h:
+        digit = w
+    else:
+        digit = h
+    if digit < i:
+        return i
+    while i<digit:
+        i+=i
+    return i
+
+def image_to_28_size(image):
+    size = closiest_28(image.shape[0], image.shape[1])
+    ar = np.zeros((size, size))
+    highFrom = (int)((size - image.shape[0]) / 2)
+    widthFrom = (int)((size - image.shape[1]) / 2)
+    w = 0
+    for i in range(len(ar)):
+        h = 0
+        for j in range(len(ar[i])):
+            if (highFrom <= i) & (widthFrom <= j) & (h < image.shape[1]) & (w < image.shape[0]):
+                ar[i][j] = image[w][h]
+                h += 1
+        if (highFrom <= i):
+            w += 1
+    return ar
 
 # load and prepare the image
-def load_image(filename):
-	# load the image
-	img = load_img(filename, target_size=(28, 28), color_mode = "grayscale")
-	# convert to array
-	img = img_to_array(img)
-	# reshape into a single sample with 1 channel
-	img = img.reshape(1, 28, 28, 1)
-	# prepare pixel data
-	img = img.astype('float32')
-	img = img / 255.0
-	return img
+def load_image2(filename):
+    image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    resized = cv2.resize(image_to_28_size(image), (28, 28), interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite('resized.jpg', resized)
+    resized = resized.reshape(1, 28, 28, 1)
+    # prepare pixel data
+    resized = resized.astype('float32')
+    resized = resized / 255.0
+    return resized
 
 # load an image and predict the class
 def run_example():
-	# load the image
-	img = load_image('digit.jpg')
-	# load model
-	model = load_model('final_model.h5')
-	# predict the class
-	digit = model.predict(img, 32, 0)
-	digit = digit[0]
-	strings = []
-	for number in range(10):
-		d = digit[number]
-		if d>0.1:
-			strings.append(str(number)+"="+str(d)[0:3])
-	return strings
-
-# entry point, run the example
-run_example()
+    # load the image
+    img = load_image2('digit.jpg')
+    # load model
+    model = load_model('final_model.h5')
+    # predict the class
+    digit = model.predict(img, 128, 0)
+    digit = digit[0]
+    strings = []
+    for number in range(10):
+        d = digit[number]
+        if d > 0.1:
+            strings.append(str(number) + "=" + str(d)[0:3])
+    return strings
